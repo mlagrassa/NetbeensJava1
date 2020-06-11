082542d438cc792841bed44c2c37c96c32c397eb
mlagrassa <marco.lagrassa@itisarezzo.it>
2020-06-11 09:31:49 +0200

    2 uesempio

--- Desktop/gitNetbeens1/src/gitnetbeens1/GitNetbeens1.java
+++ Desktop/gitNetbeens1/src/gitnetbeens1/GitNetbeens1.java
@@ -17,6 +17,8 @@ public class GitNetbeens1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("rr");
+        //2
+        System.out.println("bbb");
     }
     
 }
